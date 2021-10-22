d25150488a164198727b4d938e9da2281c03af1b
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2021-10-22 19:07:20 +0700

    Update code_eda.R

--- code_eda.R
+++ code_eda.R
@@ -9,7 +9,7 @@ movies <- read_csv("MoviesOnStreamingPlatforms_updated.csv")
 movies$Age<- movies$Age%>%replace(is.na(movies$Age),"all")
 movies$Directors <- movies$Directors%>%replace(is.na(movies$Directors),"UNKNOWN")
 movies$IMDb<- movies$IMDb%>% replace(is.na(movies$IMDb),"0/10")
-movies$`Rotten Tomatoes`<- movies$`Rotten Tomatoes`%>% replace(is.na(movies$`Rotten Tomatoes`),"0/100")
+movies$`Rotten.Tomatoes`<- movies$`Rotten.Tomatoes`%>% replace(is.na(movies$`Rotten.Tomatoes`),"0/100")
 movies$Country<- movies$Country%>%replace(is.na(movies$Country),"UNKNOWN")
 movies$Runtime<- movies$Runtime%>%replace(is.na(movies$Runtime),0)
 movies$Genres <- movies$Genres%>%replace(is.na(movies$Genres),"UNKNOWN")
@@ -17,9 +17,9 @@ movies$Language <- movies$Language%>%replace(is.na(movies$Language),"UNKNOWN")
 # drop column TYPE
 movies <- select(movies,-c(Type))
 # drop column ..1
-movies<- select(movies,-c(...1))
+movies<- select(movies,-c(X))
 # format score
-movies$`Rotten Tomatoes`<- movies$`Rotten Tomatoes`%>%str_remove("/100")%>%str_trim()%>%as.numeric()
+movies$`Rotten.Tomatoes`<- movies$`Rotten.Tomatoes`%>%str_remove("/100")%>%str_trim()%>%as.numeric()
 movies$IMDb <- movies$IMDb%>% str_remove("/10")%>%str_trim()%>%as.numeric()
 
 #null checking
@@ -38,7 +38,7 @@ movies%>%duplicated()%>% table()
 IMDb_avg<-(movies%>% select(IMDb)%>% filter(movies$IMDb>0)%>%sum() ) / count(movies)
 print(IMDb_avg)
 #2
-rotten_avg<-(movies%>% select(`Rotten Tomatoes`)%>% filter(movies$`Rotten Tomatoes`>0)%>%sum() ) / count(movies)
+rotten_avg<-(movies%>% select(`Rotten.Tomatoes`)%>% filter(movies$`Rotten.Tomatoes`>0)%>%sum() ) / count(movies)
 print(rotten_avg)
 #3
 movie_90 <- movies%>%select(Year)%>%filter(movies$Year<=2000 & movies$Year>=1990)%>%count()
